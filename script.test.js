bbf7fe6cb1d6218acd5736107440b262950aea74
rasyidannas <rasshit.dsgn@gmail.com>
2024-08-03 14:05:24 +0700

    6/11. Setting up jest.mp4

---
+++ script.test.js
@@ -0,0 +1,14 @@
+const googleSearch = require("./script");
+
+dbMock = ["dog.com", "cheesepuff.com", "disney.com", "dogpictures.com"];
+
+// it() that's used to define an individual test.
+it("this is a silly test", () => {
+  expect("hello").toBe("hello");
+  //   googleSearch("testtest", dbMock);
+});
+
+it("is searching google", () => {
+  expect(googleSearch("testtest", dbMock)).toEqual([]);
+  expect(googleSearch("dog", dbMock)).toEqual(["dog.com", "dogpictures.com"]);
+});
